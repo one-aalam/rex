9caae03f092161f0145e3874f838d3850db31a15
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-04-26 21:21:05 +0530

    feat: Add easy imports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,2 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log(`core - add: ${a}, ${b}`)
-  }
-  return a + b;
-};
-export const sub = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log(`core - sub: ${a}, ${b}`)
-  }
-  return a - b;
-};
+export * from './vdom';
+export * from './store';
