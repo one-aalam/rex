38a494f21974ad7281626690ee8d4a2e31db7db5
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2020-04-27 22:23:19 +0530

    chore: Add methods to animate route (commented)

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -7,6 +7,7 @@ export const createRouter: any = (options: any = { hash: false, routes: [], notF
   let _routes: Route[] = options.routes.map((route: any) => addRoute(route.fragment, route.component))
   let lastPathname: string;
   let notFoundComponent:any = options.notFound || noop;
+  let _outlet = options.outlet
 
   const navigate = (fragment: string) => !options.hash && !!(history.pushState) ? navigateHistory(fragment) : navigateHash(fragment) ;
   const getPath = () => {
@@ -24,8 +25,27 @@ export const createRouter: any = (options: any = { hash: false, routes: [], notF
     }
   }
 
+
+  // function animate(oldContent: any, newContent: any) {
+  //   oldContent.style.position = 'absolute';
+
+  //   oldContent.animate({
+  //     opacity: [1, 0]
+  //   }, 1000);
+
+  //   var fadeIn = newContent.animate({
+  //     opacity: [0, 1]
+  //   }, 1000);
+
+  //   fadeIn.onfinish = function() {
+  //     oldContent.parentNode.removeChild(oldContent);
+  //   };
+  // }
+
   const checkRoutes = () => {
-    const pathname = getPath(); console.log(pathname)
+    const pathname = getPath();
+
+    // @TODO: Emit custom events (pre)
     if (options.before && typeof options.before === 'function') {
       options.before({
         from: lastPathname,
@@ -35,20 +55,39 @@ export const createRouter: any = (options: any = { hash: false, routes: [], notF
     if (lastPathname === pathname) {
       return
     }
+    // const prevRoute = _routes.find(route => {
+    //   const { testRegExp } = route;
+    //   return testRegExp.test(lastPathname);
+    // })
     lastPathname = pathname;
-
     const currentRoute = _routes.find(route => {
       const { testRegExp } = route;
       return testRegExp.test(pathname);
     })
 
     if (!currentRoute) {
-      notFoundComponent()
+      _outlet.innerHTML = notFoundComponent()
       return
     }
     const urlParams = extractUrlParams(currentRoute, pathname)
 
-    currentRoute.component(urlParams);
+    // @TODO: Add route guards
+    // @TODO: Check resolve cb presence for making API calls based on parameters
+
+    var wrapper = document.createElement('div');
+        // @ts-ignore
+        wrapper.innerHTML = currentRoute.component(urlParams);
+
+        // var oldContent = document.querySelector('.cc');
+        // var newContent = wrapper.querySelector('.cc');
+
+        _outlet.innerHTML = currentRoute.component(urlParams);
+        // _outlet.appendChild(newContent);
+        // if (oldContent) {
+        //   animate(oldContent, newContent);
+        // }
+
+    // @TODO: Emit custom events (post routing)
     if (options.after && typeof options.after === 'function') {
       options.after({
         path: lastPathname,
